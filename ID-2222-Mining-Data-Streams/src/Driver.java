9af21cf5c1422af1383b83e10edce7642d7c477f
Lazarko <lazarc@kth.se>
2020-11-19 15:01:23 +0100

    HLL UPDATE

--- ID-2222-Mining-Data-Streams/src/Driver.java
+++ ID-2222-Mining-Data-Streams/src/Driver.java
@@ -1,16 +1,33 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Driver {
     //Constants
     private static final String filename = "web-Google.txt";
-
+    private static final double alpha = 0.697;
+    private final int b = 5;
     public static void main(String[] args) throws FileNotFoundException {
-       // ArrayList<Integer> inputStream = readFile();
-        System.out.println(     Integer.numberOfLeadingZeros(203050));
-        System.out.println(     Integer.numberOfLeadingZeros(0000000101010101));
+        HyperLogLog hll = new HyperLogLog(64);
+        ArrayList<Integer> inputStream = readFile();
+
+        for(int i = 0; i < inputStream.size(); i++){
+            //System.out.println(Arrays.toString(hll.add(inputStream.get(i))));
+            hll.add(inputStream.get(i));
+            if((i % 1000) == 1){
+                System.out.println(hll.size(alpha));
+            }
+        }
+
+
+
+
+
+
+
 
 
     }
--- ID-2222-Mining-Data-Streams/src/HyperLogLog.java
+++ ID-2222-Mining-Data-Streams/src/HyperLogLog.java
@@ -1,69 +1,46 @@
 public class HyperLogLog {
 
-    private final Long LARGE_PRIME =  948701839L;
-    private final Long LARGE_PRIME2 = 6920451961L;
-    private int tail_size;
+    private int[] counter;
+    private int tail;
 
 
 
-    public HyperLogLog(int l){
-        tail_size = l;
-    }
-
-    /**
-     * TEST HASH FUNKTION (lånat från stackoverflow). TODO: MÅSTE SKRIVAS OM DET MÅSTE VA B = LOG2 (M) hashning
-     * @param val
-     * @return
-     */
-    private int hash(int val){
-        long scaled = (long) val * LARGE_PRIME;
-
-        // Fill in the lower bits
-        long shifted = scaled + LARGE_PRIME2;
-
-        // Add to the lower 32 bits the upper bits which would be lost in
-        // the conversion to an int.
-        long filled = shifted + ((shifted & 0xFFFFFFFF00000000L) >> 32);
-
-        // Pare it down to 31 bits in this case.  Replace 7 with F if you
-        // want negative numbers or leave off the `& mask` part entirely.
-        int masked = (int) (filled & 0x7FFFFFFF);
-        return masked;
+    public HyperLogLog(int m){
+        tail = m;
+        counter = new int[m];
 
     }
 
-    /**
-     * i cannot exceed 255, TODO: MAKE SURE IT CAN
-     * Counts the number of leading zeros
-     * @return
-    private int rho(int i) {
 
-        int lz = 8;
-        Integer.numberOfLeadingZeros(i);
-        while(i)
-        {
-            lz--;
-            i >>>= 1;
+
+    private String truncate(int item){
+        int hash = Integer.hashCode(item);
+        return String.format("%"+tail+"s", Integer.toBinaryString(hash)).replace(' ', '0');
+    }
+
+    public int[] add(int item){
+        String hashed = truncate(item);
+
+        int index = 0;
+        for(int i = 0; i < hashed.length(); i++){
+            if(hashed.charAt(hashed.length()-1-i) == '1'){
+                index = i;
+            }
         }
-        return lz;
-    }
-     */
-
-
-    public void add(int[] counter, int item){
-        int index = hash(item);
-        //int clz = rho(index);
-        int clz = Integer.numberOfLeadingZeros(index); //TODO: JAG TROR DEN FUNKAR
-        if(clz > counter[index]) {
-            counter[index] = clz;
+        int leading = (index + 1);
+        if(leading > counter[index]) {
+            counter[index] = leading;
         }
-
-
+        return counter;
     }
 
 
-    public void size( ){
-
+    public double size(double alpha) {
+        float sum = 0;
+        for (int i : counter) {
+            sum += Math.pow(2, -i);
+        }
+        return  Math.pow(sum, -1)*alpha*Math.pow(counter.length, 2);
     }
 
 
