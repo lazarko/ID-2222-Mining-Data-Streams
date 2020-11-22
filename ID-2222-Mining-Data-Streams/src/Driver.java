2f2108e643a70b09ac4f5a189e1926efd9363535
BeaLove <beatriceelovely@gmail.com>
2020-11-22 10:50:17 +0100

    bytte namn på adjacency list till graph och fixade i Hyperball så arrays kan jämföras

--- ID-2222-Mining-Data-Streams/src/Driver.java
+++ ID-2222-Mining-Data-Streams/src/Driver.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.security.DigestException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -8,20 +9,22 @@ import java.security.MessageDigest;
 
 public class Driver {
     //Constants
-    private static final String filename = "web-Google.txt";
+    private static final String filename = "CA-HepTh.txt";
     private static final int b = 4;
-    public static void main(String[] args) throws FileNotFoundException, NoSuchAlgorithmException {
+    public static HashMap<Integer, ArrayList<Integer>> graph = new HashMap<>(); //hashmap of nodes and their edges.
+
+    public static void main(String[] args) throws FileNotFoundException, NoSuchAlgorithmException, DigestException {
         HyperLogLog hll = new HyperLogLog(b);
         ArrayList<Integer> inputStream = readFile();
         int[] counter = new int[(int)Math.pow(2,b)];
         for(int i = 0; i < inputStream.size(); i++){
-            //System.out.println(Arrays.toString(hll.add(inputStream.get(i))));
+
             counter = hll.add(counter, inputStream.get(i));
-            //if((i % 1000) == 1){
-                //System.out.println(hll.size());
-            //}
+
         }
         System.out.println(hll.size(counter));
+
+        HyperBall hb = new HyperBall(graph, b); //anropar och utför hyperball operationen
 
 
     }
@@ -42,24 +45,24 @@ public class Driver {
             }
 
         }
-        HashMap<Integer, ArrayList<Integer>> adjacency_list = new HashMap<>();
-        adjacency_list.put(Integer.parseInt(edgeList.get(0)[0]), new ArrayList<>(Integer.parseInt(edgeList.get(1)[1])));
+
+        graph.put(Integer.parseInt(edgeList.get(0)[0]), new ArrayList<>(Integer.parseInt(edgeList.get(1)[1])));
         for (int i = 1; i < edgeList.size(); i++){
 
             String[] edge = edgeList.get(i);
             if (!edge[0].equals(edgeList.get(i-1)[0])){
                 ArrayList<Integer> connected_nodes = new ArrayList<>();
                 connected_nodes.add(Integer.parseInt(edge[1]));
-                adjacency_list.put(Integer.parseInt(edge[0]), connected_nodes);
+                graph.put(Integer.parseInt(edge[0]), connected_nodes);
             }
             else{
                 int node = Integer.parseInt(edge[0]);
-                ArrayList<Integer> list = adjacency_list.get(node);
+                ArrayList<Integer> list = graph.get(node);
                 list.add(Integer.parseInt(edge[1]));
-                adjacency_list.put(node, list);
+                graph.put(node, list);
             }
         }
-        //adjacency_list.entrySet().forEach(entry -> System.out.println(entry.getKey() + "-->" + entry.getValue()));
+        graph.entrySet().forEach(entry -> System.out.println(entry.getKey() + "-->" + entry.getValue()));
 
 
         sc.close();
